0d962e7bc6fd897ce0dfd448a3bc1e964418da60
shdandu <shdandu@unc.edu>
2026-01-16 14:01:30 -0500

    finished exercise 01

---
+++ exercises/ex01_tea_party.py
@@ -0,0 +1,30 @@
+"""Determines number of tea bags, treats, and cost of a tea party based on the number of people attending"""
+
+__author__ = "730705811"
+
+
+def main_planner(guests: int) -> None:
+    """Returns information about tea party planning"""
+    print("A Cozy Tea Party for " + str(guests) + " People!")
+    print("Tea Bags: " + str(tea_bags(people=guests)))
+    print("Treats: " + str(treats(people=guests)))
+    print("Cost: $" + str(cost(tea_bags(people=guests), treats(people=guests))))
+
+
+def tea_bags(people: int) -> int:
+    """Determines number of tea bags needed"""
+    return people * 2
+
+
+def treats(people: int) -> int:
+    """Determines number of treats needed"""
+    return int(tea_bags(people=people) * 1.5)
+
+
+def cost(tea_count: int, treat_count: int) -> float:
+    """Determines cost of tea party"""
+    return tea_count * 0.50 + treat_count * 0.75
+
+
+if __name__ == "__main__":
+    main_planner(guests=int(input("How many guests are attending your tea party? ")))
